ef5cfa03de67a2895680bbf2ecfad9d53e1e1eec
felipe7 <phillipi.cardoso@discente.ufj.edu.br>
2024-06-20 11:29:37 -0300

    Add files via upload

---
+++ Desafios/Restaurante Universitário/src/cardapioru/src/main/java/ru/cardapioru/Cardapioru.java
@@ -0,0 +1,19 @@
+
+
+package ru.cardapioru;
+
+/**
+ *
+ * @author lipe
+ */
+public class Cardapioru {
+
+    public static void main(String[] args) {
+   
+        InterfacePrincipal i = new InterfacePrincipal();
+        
+        // Define que a janela seja visível
+        i.setVisible(true);
+        
+    }
+}
