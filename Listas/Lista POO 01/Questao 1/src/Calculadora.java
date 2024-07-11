553d52c390274b13fe66523de4106f9f41cb99bb
felipe7 <phillipi.cardoso@discente.ufj.edu.br>
2024-07-11 10:26:25 -0300

    Delete Listas directory

--- Listas/Lista POO 01/Questao 1/src/Calculadora.java
+++
@@ -1,8 +0,0 @@
-public abstract class Calculadora {
-    public abstract float soma(float a, float b);
-    public abstract float subtracao(float a, float b);
-    public abstract float multiplicacao(float a, float b);
-    public abstract float divisao(float a, float b);
-}
-
-
